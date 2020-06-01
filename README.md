e9f0a4044ab9b71ae850bdb558550b8ebf44a256
Maher Abuthraa <maher.abuthraa@gmail.com>
2020-06-01 17:11:03 +0200

    [UPDATE] update README [UPDATE] Rename UI components into NoteList and NoteDetail

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # my-note-android
-MyNote app is sample of Android app which written in Kotlin and follow MVVM with DI Koen
+MyNote app is sample of Android app which written in Kotlin and follow MVVM with DI Koin
+
+## 1. Dependencies:
+//- MVVM (Architecture)
+//- Android architecture components (LiveData,LifeCycle, & viewModel)
+//- Android Navigation (FLOW)
+//- Koin (DI)
+//- Retrofit 2 (HTTP API)
+//- RxKotlin (Reactive programming)
+//- Coroutines (for non-blocking programming)
+//- Moshi (lib to parse JSON into model)
+//- Epoxy
+
--- app/src/main/java/com/apipas/mynote/NoteListFragment.kt
+++ app/src/main/java/com/apipas/mynote/NoteListFragment.kt
@@ -8,24 +8,20 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class FirstFragment : Fragment() {
+class NoteListFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_note_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            findNavController().navigate(R.id.action_NoteListFragment_to_NoteDetailFragment)
         }
     }
 }
