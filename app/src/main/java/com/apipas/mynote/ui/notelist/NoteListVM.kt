dc500aab6378d7f7b26c9a90cdcec555a6af4673
Maher Abuthraa <maher.abuthraa@gmail.com>
2020-06-08 14:01:36 +0200

    [NF] Add SimpleRecyclerAdapter [NF] Add bindingAdapter bindItems to support dynamic RecyclerView with VM.ItemListRes and Items as Observable List [NF] Add RecyclerView to NoteList [NF] Pass real entity to NoteDetail

--- app/src/main/java/com/apipas/mynote/ui/notelist/NoteListVM.kt
+++ app/src/main/java/com/apipas/mynote/ui/notelist/NoteListVM.kt
@@ -1,27 +1,33 @@
 package com.apipas.mynote.ui.notelist
 
+import androidx.databinding.ObservableArrayList
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.OnLifecycleEvent
+import com.apipas.mynote.data.model.Note
 import com.apipas.mynote.ui.base.BaseViewModel
 import kotlinx.coroutines.delay
 import java.util.*
 
 class NoteListVM : BaseViewModel() {
-
+    val notes = ObservableArrayList<Note>()
     val dataTest = MutableLiveData<String>().apply { value = "No Data " }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
     private fun onResume() {
         launchDataLoad {
             delay(2000)
+
+            val arr = Array<Note>(100) { i -> Note("title #$i", "content #$i") }
+            val arrayList = ArrayList<Note>(arr.toList())
+            notes.addAll(arrayList)
             dataTest.value = "New Data ${Date()}"
 //            throw Exception("test")
         }
     }
 
-    fun goToNoteDetail(content: String) {
-        publish(NavToNoteDetail(content))
+    fun goToNoteDetail(note: Note) {
+        publish(NavToNoteDetail(note))
     }
 }
